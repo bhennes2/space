daa360db5b94111d9f4b1bbce748c5077e10b1af
bhennes2 <bhennes2@gmail.com>
2015-12-11 16:56:56 -0600

    Added Angular resource

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var app = angular.module('space', []);
 
 app.controller("Main", function($scope){
-
   $scope.today = new Date();
-
 });
 
+app.factory("Page", function($resource){
+  return $resource('/pages/:id', { id: '@id' });
+});
