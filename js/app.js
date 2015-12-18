00a62e52fb5dfd136b01f3c2b6df9e8e695e383c
bhennes2 <bhennes2@gmail.com>
2015-12-18 12:10:13 -0600

    Added basic Angular resource

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-var app = angular.module('space', []);
+var app = angular.module('space', ['ngResource']);
 
-app.controller("Main", function($scope){
-  $scope.today = new Date();
-});
+app.config(['$resourceProvider', function($resourceProvider) {
+  // Don't strip trailing slashes from calculated URLs
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+}]);
 
 app.factory("Page", function($resource){
-  return $resource('/pages/:id', { id: '@id' });
+  return $resource('https://space-engine.herokuapp.com/locomotive/space/pages/:id.json', { id: '@id' });
+});
+
+app.controller("Main", function($scope, Page){
+  $scope.today = new Date();
+  console.log(Page.query());
 });
