0af0dc71a63116fbb93a1a62130ee3ec218eff26
bhennes2 <bhennes2@gmail.com>
2016-01-06 09:20:31 -0600

    Set up Angular to connect to Locomotive API

---
+++ coal.rb
@@ -0,0 +1,6 @@
+require 'locomotive/coal'
+
+client = Locomotive::Coal::Client.new('https://space-engine.herokuapp.com', { email: 'dev@launchpadlab.com', api_key: 'b72fa5406790fec0ab70a649a84aed2f5841c8f5' })
+site = client.sites.by_handle('space')
+site_client = client.scope_by(site)
+puts site_client.contents.missions.all.inspect
--- js/app.js
+++ js/app.js
@@ -1,15 +1,45 @@
-var app = angular.module('space', ['ngResource']);
+var app = angular.module('space', ['ngResource', 'ngSanitize']);
 
-app.config(['$resourceProvider', function($resourceProvider) {
-  // Don't strip trailing slashes from calculated URLs
-  $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
-
-app.factory("Page", function($resource){
-  return $resource('https://space-engine.herokuapp.com/locomotive/space/pages/:id.json', { id: '@id' });
+app.factory("CMS", function($resource){
+  return $resource('/proxy/locomotive/api/v3/:path/:content_type/:subpath', {},
+    {
+      missions: {
+        method: 'GET',
+        isArray: true,
+        params: {
+          path: 'content_types',
+          'content_type': 'missions',
+          subpath: 'entries.json'
+        },
+        headers: {
+          "X-Locomotive-Account-Email": "dev@launchpadlab.com",
+          "X-Locomotive-Account-Token": "UTqEUsnvqq8aGtG131sb",
+          "X-Locomotive-Site-Handle": "space"
+        }
+      },
+      token: {
+        method: 'POST',
+        params: {
+          path: 'tokens.json'
+        },
+        cache: false,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+        }
+      }
+    }
+  );
 });
 
-app.controller("Main", function($scope, Page){
+app.controller("Main", function($scope, CMS){
   $scope.today = new Date();
-  console.log(Page.query());
+
+  $scope.missions = CMS.missions();
+
 });
